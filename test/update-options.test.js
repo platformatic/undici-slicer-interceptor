d62cb4241a92433aabb3e37cf3d382d22d62740d
Ivan Tymoshenko <ivan@tymoshenko.me>
2025-05-29 15:00:50 +0200

    fixup

--- test/update-options.test.js
+++ test/update-options.test.js
@@ -6,7 +6,7 @@ import { once } from 'node:events'
 import { createInterceptor, updateInterceptorOptions } from '../index.js'
 
 describe('make-cacheable-interceptor - update options', () => {
-  test('should update interceptor options', async () => {
+  test('should update interceptor options', async (t) => {
     // Setup test server
     const server = createServer((req, res) => {
       res.end('hello world')
@@ -15,73 +15,71 @@ describe('make-cacheable-interceptor - update options', () => {
     server.listen(0)
     await once(server, 'listening')
 
+    t.after(() => { server.close() })
+
     const serverUrl = `http://localhost:${server.address().port}`
     const hostname = `localhost:${server.address().port}`
 
-    try {
       // Create agent with our interceptor
-      const agent = new Agent()
-      const interceptor = createInterceptor({
-        rules: [
-          { routeToMatch: `${hostname}/test-1`, headers: { 'test-header': 'test-1' } },
-          { routeToMatch: `${hostname}/test-2`, headers: { 'test-header': 'test-2' } }
-        ]
-      })
+   const agent = new Agent()
+   const interceptor = createInterceptor({
+     rules: [
+       { routeToMatch: `${hostname}/test-1`, headers: { 'test-header': 'test-1' } },
+       { routeToMatch: `${hostname}/test-2`, headers: { 'test-header': 'test-2' } }
+     ]
+   })
 
-      const composedAgent = agent.compose(interceptor)
-      setGlobalDispatcher(composedAgent)
+   const composedAgent = agent.compose(interceptor)
+   setGlobalDispatcher(composedAgent)
 
-      {
-        const test1Res = await request({
-          method: 'GET',
-          origin: serverUrl,
-          path: '/test-1'
-        })
+   {
+     const test1Res = await request({
+       method: 'GET',
+       origin: serverUrl,
+       path: '/test-1'
+     })
 
-        assert.strictEqual(test1Res.headers['test-header'], 'test-1')
-        await test1Res.body.dump()
-      }
+     assert.strictEqual(test1Res.headers['test-header'], 'test-1')
+     await test1Res.body.dump()
+   }
 
-      updateInterceptorOptions(interceptor, {
-        rules: [
-          { routeToMatch: `${hostname}/test-1`, headers: { 'test-header': 'test-42' } },
-          { routeToMatch: `${hostname}/test-3`, headers: { 'test-header': 'test-3' } }
-        ]
-      })
+   updateInterceptorOptions(interceptor, {
+     rules: [
+       { routeToMatch: `${hostname}/test-1`, headers: { 'test-header': 'test-42' } },
+       { routeToMatch: `${hostname}/test-3`, headers: { 'test-header': 'test-3' } }
+     ]
+   })
 
-      {
-        // Route config was modified
-        const test1Res = await request({
-          method: 'GET',
-          origin: serverUrl,
-          path: '/test-1'
-        })
+   {
+     // Route config was modified
+     const test1Res = await request({
+       method: 'GET',
+       origin: serverUrl,
+       path: '/test-1'
+     })
 
-        assert.strictEqual(test1Res.headers['test-header'], 'test-42')
-        await test1Res.body.dump()
+     assert.strictEqual(test1Res.headers['test-header'], 'test-42')
+     await test1Res.body.dump()
 
-        // Route config was removed
-        const test2Res = await request({
-          method: 'GET',
-          origin: serverUrl,
-          path: '/test-2'
-        })
+     // Route config was removed
+     const test2Res = await request({
+       method: 'GET',
+       origin: serverUrl,
+       path: '/test-2'
+     })
 
-        assert.strictEqual(test2Res.headers['test-header'], undefined)
-        await test2Res.body.dump()
+     assert.strictEqual(test2Res.headers['test-header'], undefined)
+     await test2Res.body.dump()
 
-        // Route config was added
-        const test3Res = await request({
-          method: 'GET',
-          origin: serverUrl,
-          path: '/test-3'
-        })
+     // Route config was added
+     const test3Res = await request({
+       method: 'GET',
+       origin: serverUrl,
+       path: '/test-3'
+     })
 
-        assert.strictEqual(test3Res.headers['test-header'], 'test-3')
-        await test3Res.body.dump()
-      }
-    } finally {
-      server.close()
-    }
+     assert.strictEqual(test3Res.headers['test-header'], 'test-3')
+     await test3Res.body.dump()
+   }
   })
 })
